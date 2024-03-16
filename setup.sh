f66399feec604eade4300721ddb591f4b5be3587
MR-HIFZU <siddafreen786@gmail.com>
2024-03-16 03:01:40 +0000

    phishing

---
+++ setup.sh
@@ -0,0 +1,209 @@
+#RED="$(printf 
+#'\033[31m')"################################banner########################################### 
+#echo -e "\e[31mDOING SOMETHING IMPORTENT THIS WILL TAKE SOMETIME .. THE PROCESS WILL TAKE SOME TIME
+
+#echo -e" THIS PROCESS WILL TAKE 30 MINUTS TO DONE\ r u agree to continue  "
+
+
+echo -e "This process will take 30 minutes to complete.\nAre you sure you want t>
+read -r userInput
+
+if [[ "$userInput" =~ ^[Yy]$ ]]; then
+  echo "ONLY FOR SYNC.MUZZAMMIL & SYNC HACK
+   sleep 4
+
+  # Replace this with your actual 30-minute process
+ # sleep 1800  # Simulates a 30-minute process (30 * 60 seconds)
+else
+  echo "Exiting..."
+fi
+
+
+
+
+sudo apt install figlet -y 
+
+git clone https://github.com/GManOfficial/Termux_HackingLab_Setup.git
+
+ cd Termux_HackingLab_Setup
+
+ pip3 install -r requirements.txt
+
+ python3 install.py
+
+
+rm -rf Termux_HackingLab_Setup
+
+echo ONLY FOR MY STUDENTS MUZZAMIL AND @sync.xez | lolcat
+
+mkdir -p ~/.local/share/fonts/figlet-fonts/ 
+git clone https://github.com/xero/figlet-fonts.git ~/.local/share/fonts/figlet-fonts
+pip install lolcat
+figlet -c -f ~/.local/share/fonts/figlet-fonts/'ANSI Shadow.flf' MR HIFZU | lolcat
+read -p "Press Enter to continue..."
+
+
+#===============================================================
+echo "tool 1 cloning "
+git clone https://github.com/princekrvert/Ravana.git
+#===============================================================
+echo "tool 2 cloning"
+git clone https://github.com/Cyber-Anonymous/Dark-Phish.git
+#===============================================================
+echo "tool 3 cloning "
+git clone https://github.com/rubikproxy/rubikphish.git
+#===============================================================
+echo "tool 4 cloning"
+git clone https://github.com/GemGeorge/SniperPhish.git
+#===============================================================
+echo "tool 5 cloning"
+git clone https://github.com/ElliotAlderson51/Fsociety-Phishing-Tool.git
+#===============================================================
+echo "tool 6 cloneing"
+git clone --depth=1 https://github.com/Th3-C0der/Th3-Phisher
+#===============================================================
+echo "tool 7 cloneing"
+git clone https://github.com/cipheras/cipherginx.git
+#===============================================================
+echo "tool 8 cloneing"
+git clone https://github.com/wifiphisher/wifiphisher.git
+#===============================================================
+echo "tool 9 cloneing"
+git clone https://github.com/gophish/gophish.git
+#===============================================================
+echo "tool 10 cloneing" 
+git clone -b Termux-Support-Branch https://github.com/syed-roshan01/HiddenEye.git
+echo "10 tools cloned"
+#===============================================================
+echo "tool 11 cloneing"
+git clone https://github.com/EricksonAtHome/blackeye.git
+#===============================================================
+echo "tool 12 cloneing"
+git clone https://github.com/Ignitetch/AdvPhishing.git
+#===============================================================
+echo "tool 13 cloneing"
+git clone https://github.com/UndeadSec/SocialFish.git
+#===============================================================
+echo "tool 14 cloneing"
+git clone https://github.com/rsmusllp/king-phisher.git
+#===============================================================
+echo "tool 15 cloneing"
+git clone https://github.com/richardsonjf/shellphish.git
+#===============================================================
+echo "tool 16 cloneing"
+git clone https://github.com/rezaaksa/PhishX.git
+#===============================================================
+echo "tool 17 cloneing"
+git clone https://github.com/rezaaksa/PhishX.git
+#===============================================================
+echo "tool 18 cloneing"
+git clone https://github.com/Akshay-Arjun/69phisher.git
+#===============================================================
+echo "tool 19 cloneing"
+git clone https://github.com/hackerxphantom/XPHISHER.git
+#===============================================================
+echo "tool 20 cloneing"
+git clone https://github.com/hackerxphantom/XPHISHER.git
+echo "20 tools cloned"
+#===============================================================
+echo "tool 21 cloneing"
+git clone https://github.com/noob-hackers/sneakphish
+#===============================================================
+echo "tool 22 cloneing"
+git clone https://github.com/rabikishanr/x-zone.git
+#===============================================================
+echo "tool 23 cloneing"
+git clone https://github.com/LiNuX-Mallu/CAM-DUMPER.git
+#===============================================================
+echo "tool 24 cloneing"
+git clone https://github.com/Jasutinn/WebCamPhishing.git
+#===============================================================
+echo "tool 25 cloneing"
+git clone https://github.com/XPH4N70M/HACK-CAMERA.git
+#===============================================================
+echo "tool 26 cloneing"
+https://github.com/Th30neAnd0nly/TrackDown.git
+#===============================================================
+echo "tool 27 cloneing"
+git clone https://github.com/Viralmaniar/I-See-You.git
+#===============================================================
+echo "tool 28 cloneing"
+git clone https://github.com/Viralmaniar/I-See-You.git
+#===============================================================
+echo "tool 29 cloneing"
+git clone https://github.com/Viralmaniar/I-See-You.git
+#===============================================================
+echo "tool 30 cloneing"
+git clone https://github.com/ultrasecurity/Storm-Breaker.git
+echo "30 tools cloned "
+#===============================================================
+clear
+
+pip install colorama
+
+pip install psutil clear 
+
+echo "installing some packegs this will take time go and drink a coffee" 
+
+
+figlet -f slant -C upper THIS WILL TAKE TIME | lolcat 
+
+
+apt update -y 
+
+
+apt upgrade -y
+
+apt update -y 
+
+clear 
+
+pip install lolcat 
+
+
+apt install python2 -y 
+
+
+apt install ruby -y
+
+
+apt install git -y
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo "pckgs r installed now installing some libraries"
+
+pip install scapy
+apt upgrade -y 
+apt update -y 
+
+
+figlet Process done | lolcat
+
+sleep 2 
+
+echo evrything is done now exiting the terminal and comeback in termux | lolcat
+exit 
+
