f84d640449b2d5ddd226d1375348b2c30189e082
MR-HIFZU <99407134+MR-HIFZU@users.noreply.github.com>
2024-03-16 08:57:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,98 +44,98 @@ read -p "Press Enter to continue..."
 
 
 #===============================================================
-echo "tool 1 cloning "
+echo tool 1 cloning | lolcat
 git clone https://github.com/princekrvert/Ravana.git
 #===============================================================
-echo "tool 2 cloning"
+echo tool 2 cloning | lolcat
 git clone https://github.com/Cyber-Anonymous/Dark-Phish.git
 #===============================================================
-echo "tool 3 cloning "
+echo tool 3 cloning | lolcat
 git clone https://github.com/rubikproxy/rubikphish.git
 #===============================================================
-echo "tool 4 cloning"
+echo tool 4 cloning | lolcat
 git clone https://github.com/GemGeorge/SniperPhish.git
 #===============================================================
-echo "tool 5 cloning"
+echo tool 5 cloning | lolcat
 git clone https://github.com/ElliotAlderson51/Fsociety-Phishing-Tool.git
 #===============================================================
-echo "tool 6 cloneing"
+echo tool 6 cloneing | lolcat
 git clone --depth=1 https://github.com/Th3-C0der/Th3-Phisher
 #===============================================================
-echo "tool 7 cloneing"
+echo tool 7 cloneing | lolcat
 git clone https://github.com/cipheras/cipherginx.git
 #===============================================================
-echo "tool 8 cloneing"
+echo tool 8 cloneing | lolcat
 git clone https://github.com/wifiphisher/wifiphisher.git
 #===============================================================
-echo "tool 9 cloneing"
+echo tool 9 cloneing | lolcat
 git clone https://github.com/gophish/gophish.git
 #===============================================================
-echo "tool 10 cloneing" 
+echo tool 10 cloneing | lolcat
 git clone -b Termux-Support-Branch https://github.com/syed-roshan01/HiddenEye.git
-echo "10 tools cloned"
+echo 10 tools cloned | lolcat
 #===============================================================
-echo "tool 11 cloneing"
+echo tool 11 cloneing |lolcat
 git clone https://github.com/EricksonAtHome/blackeye.git
 #===============================================================
-echo "tool 12 cloneing"
+echo tool 12 cloneing|lolcat
 git clone https://github.com/Ignitetch/AdvPhishing.git
 #===============================================================
-echo "tool 13 cloneing"
+echo tool 13 cloneing|lolcat
 git clone https://github.com/UndeadSec/SocialFish.git
 #===============================================================
-echo "tool 14 cloneing"
+echo tool 14 cloneing|lolcat
 git clone https://github.com/rsmusllp/king-phisher.git
 #===============================================================
-echo "tool 15 cloneing"
+echo tool 15 cloneing|lolcat
 git clone https://github.com/richardsonjf/shellphish.git
 #===============================================================
-echo "tool 16 cloneing"
+echo tool 16 cloneing|lolcat
 git clone https://github.com/rezaaksa/PhishX.git
 #===============================================================
-echo "tool 17 cloneing"
+echo tool 17 cloneing |lolcat
 git clone https://github.com/rezaaksa/PhishX.git
 #===============================================================
-echo "tool 18 cloneing"
+echo tool 18 cloneing|lolcat 
 git clone https://github.com/Akshay-Arjun/69phisher.git
 #===============================================================
-echo "tool 19 cloneing"
+echo tool 19 cloneing|lolcat
 git clone https://github.com/hackerxphantom/XPHISHER.git
 #===============================================================
-echo "tool 20 cloneing"
+echo tool 20 cloneing|lolcat
 git clone https://github.com/hackerxphantom/XPHISHER.git
-echo "20 tools cloned"
+echo 20 tools cloned|
 #===============================================================
-echo "tool 21 cloneing"
+echo tool 21 cloneing|lolcat
 git clone https://github.com/noob-hackers/sneakphish
 #===============================================================
-echo "tool 22 cloneing"
+echo tool 22 cloneing|lolcat
 git clone https://github.com/rabikishanr/x-zone.git
 #===============================================================
-echo "tool 23 cloneing"
+echo tool 23 cloneing|lolcat
 git clone https://github.com/LiNuX-Mallu/CAM-DUMPER.git
 #===============================================================
-echo "tool 24 cloneing"
+echo tool 24 cloneing|lolcat
 git clone https://github.com/Jasutinn/WebCamPhishing.git
 #===============================================================
-echo "tool 25 cloneing"
+echo tool 25 cloneing|lolcat
 git clone https://github.com/XPH4N70M/HACK-CAMERA.git
 #===============================================================
-echo "tool 26 cloneing"
+echo tool 26 cloneing|lolcat
 https://github.com/Th30neAnd0nly/TrackDown.git
 #===============================================================
-echo "tool 27 cloneing"
+echo tool 27 cloneing|lolcat
 git clone https://github.com/Viralmaniar/I-See-You.git
 #===============================================================
-echo "tool 28 cloneing"
+echo tool 28 cloneing |lolcat
 git clone https://github.com/Viralmaniar/I-See-You.git
 #===============================================================
-echo "tool 29 cloneing"
+echo tool 29 cloneing | lolcat
 git clone https://github.com/Viralmaniar/I-See-You.git
 #===============================================================
-echo "tool 30 cloneing"
+echo tool 30 cloneing | lolcat
 git clone https://github.com/ultrasecurity/Storm-Breaker.git
-echo "30 tools cloned "
+echo 30 tools cloned | lolcat
 #===============================================================
 clear
 
@@ -143,7 +143,7 @@ pip install colorama
 
 pip install psutil clear 
 
-echo "installing some packegs this will take time go and drink a coffee" 
+echo installing some packegs this will take time go and drink a coffee | lolcat
 
 
 figlet -f slant -C upper THIS WILL TAKE TIME | lolcat 
@@ -193,7 +193,7 @@ apt install wcalc -y
 apt install openssl
 apt install bmon -y
 
-echo "pckgs r installed now installing some libraries"
+echo pckgs r installed now installing some libraries | lolcat
 
 pip install scapy
 apt upgrade -y 
